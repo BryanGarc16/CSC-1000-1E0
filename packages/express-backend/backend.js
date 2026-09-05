872c922cd0994fc25ac308b5dd6adc4efc0e9562
BryanGarc16 <bgarcia3301@gmail.com>
2026-09-04 22:20:35 -0700

    added user search by name and id in url

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -3,14 +3,6 @@ import express from "express";
 const app = express();
 const port = 8000;
 
-app.use(express.json());
-
-app.get("/", (req, res) => {
- res.send(users);
-});
-
-
-
 const users = {
   users_list: [
     {
@@ -40,6 +32,46 @@ const users = {
     },
   ],
 };
+
+app.use(express.json());
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+
+const findUserByName = (name) => {
+  return users["users_list"].filter((user) => user["name"] === name);
+};
+
+app.get("/users", (req, res) => {
+  const name = req.query.name;
+  if (name != undefined) {
+    let result = findUserByName(name);
+    result = { users_list: result };
+    res.send(result);
+  } else {
+    res.send(users);
+  }
+});
+
+const findUserById = (id) =>
+  users["users_list"].find((user) => user["id"] === id);
+
+app.get("/users/:id", (req, res) => {
+  const id = req.params["id"]; //or req.params.id
+  let result = findUserById(id);
+  if (result === undefined) {
+    res.status(404).send("Resource not found.");
+  } else {
+    res.send(result);
+  }
+});
+
+
+
 
 
 app.listen(port, () => {
